206c426b776c70574731d4120a43a47c1ef0a8b5
ishan-kaushal-99 <157468470+ishan-kaushal-99@users.noreply.github.com>
2024-02-24 21:11:45 +0530

    Update question019.java

--- Basic 1/question019.java
+++ Basic 1/question019.java
@@ -1,6 +1,5 @@
 // Write a Java program to check whether Java is installed on your computer.
 // Expected Output
-
 // Java Version: 1.8.0_71                                                            
 // Java Runtime Version: 1.8.0_71-b15                                                
 // Java Home: /opt/jdk/jdk1.8.0_71/jre                                               
